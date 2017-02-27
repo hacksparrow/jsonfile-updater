826232bc05ff0fd9dc5fdc423c1891e2941c4b17
Hage Yaapa <captain@hacksparrow.com>
2017-02-28 01:56:43 +0530

    support dot selector

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ function Updater(jsonFilePath) {
 Updater.prototype.add = function(property, value, cb) {
   var jsonFilePath = this.jsonFilePath
   fs.access(jsonFilePath, function(err) {
-    if (err) return(err)
+    if (err) return cb(err)
     fs.readFile(jsonFilePath, 'utf8', function(err, fileContent) {
+
+      if (err) return cb(err)
+
       var pkg = JSON.parse(fileContent)
-      if (property in pkg) { return cb(new Error('Property "' + property + '" already defined')) }
-      pkg[property] = value
+      var inputProperties = property.split('.')
+      var propertyCursor = pkg
+
+      for (var i = 0; i < inputProperties.length; i++) {
+        var subProperty = inputProperties[i]
+        if (i === inputProperties.length - 1) {
+          if (subProperty in propertyCursor) { return cb(new Error('Property "' + property + '" already defined')) }
+          propertyCursor[subProperty] = value
+        } else {
+          propertyCursor[subProperty] = {}
+          propertyCursor = propertyCursor[subProperty]
+        }
+      }
+
       var updatedPackage = JSON.stringify(pkg, null, 2)
       fs.writeFile(jsonFilePath, updatedPackage, cb)
     })
@@ -24,11 +39,26 @@ Updater.prototype.add = function(property, value, cb) {
 Updater.prototype.update = function(property, value, cb) {
   var jsonFilePath = this.jsonFilePath
   fs.access(jsonFilePath, function(err) {
-    if (err) return(err)
+    if (err) return cb(err)
     fs.readFile(jsonFilePath, 'utf8', function(err, fileContent) {
+
+      if (err) return cb(err)
+
       var pkg = JSON.parse(fileContent)
-      if (!(property in pkg)) { return cb(new Error('Property "' + property + '" not defined')) }
-      pkg[property] = getFormattedValues(pkg[property], value)
+
+      var inputProperties = property.split('.')
+      var propertyCursor = pkg
+
+      for (var i = 0; i < inputProperties.length; i++) {
+        var subProperty = inputProperties[i]
+        if (i === inputProperties.length - 1) {
+          if (!(subProperty in propertyCursor)) { return cb(new Error('Property "' + property + '" not defined')) }
+          propertyCursor[subProperty] = getFormattedValues(propertyCursor[subProperty], value)
+        } else {
+          propertyCursor = propertyCursor[subProperty]
+        }
+      }
+
       var updatedPackage = JSON.stringify(pkg, null, 2)
       fs.writeFile(jsonFilePath, updatedPackage, cb)
     })
@@ -38,17 +68,33 @@ Updater.prototype.update = function(property, value, cb) {
 Updater.prototype.delete = function(properties, cb) {
   var jsonFilePath = this.jsonFilePath
   fs.access(jsonFilePath, function(err) {
-    if (err) return(err)
+    if (err) return cb(err)
     fs.readFile(jsonFilePath, 'utf8', function(err, fileContent) {
+
+      if (err) return cb(err)
+
       var pkg = JSON.parse(fileContent)
+      var propertyCursor = pkg
+
       var propertiesArray = []
       if (!Array.isArray(properties)) { propertiesArray.push(properties) }
       else propertiesArray = properties
       for (var i = 0; i < propertiesArray.length; i++) {
         var property = propertiesArray[i]
-        if (!(property in pkg)) { return cb(new Error('Property "' + property + '" not defined')) }
-        pkg[property] = undefined
+        var inputProperties = property.split('.')
+
+        for (var j = 0; j < inputProperties.length; j++) {
+          var subProperty = inputProperties[j]
+          if (!(subProperty in propertyCursor)) { return cb(new Error('Property "' + property + '" not defined')) }
+          if (j === inputProperties.length - 1) {
+            propertyCursor[subProperty] = undefined
+          } else {
+            propertyCursor = propertyCursor[subProperty]
+          }
+        }
+
       }
+
       var updatedPackage = JSON.stringify(pkg, null, 2)
       fs.writeFile(jsonFilePath, updatedPackage, cb)
     })
--- test/test.js
+++ test/test.js
@@ -4,42 +4,42 @@ const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
 const updater = require('../')
-const srcPackagePath = path.join(__dirname, 'package.json')
-const destPackagePath = path.join(__dirname, 'test.package.json')
+const srcSettingsPath = path.join(__dirname, 'settings.json')
+const destSettingsPath = path.join(__dirname, 'test.settings.json')
 
 describe('updater()', function() {
 
   beforeEach(function(done) {
-    fs.createReadStream(srcPackagePath).pipe(fs.createWriteStream(destPackagePath));
+    fs.createReadStream(srcSettingsPath).pipe(fs.createWriteStream(destSettingsPath));
     done()
   })
 
   afterEach(function(done) {
-    fs.unlink(destPackagePath, done)
+    fs.unlink(destSettingsPath, done)
   })
 
   describe('.add()', function() {
 
     it('should throw if a property is aleady defined', function(done) {
-      updater(destPackagePath).add('author', 'Hage Yaapa', function(err) {
+      updater(destSettingsPath).add('author', 'Hage Yaapa', function(err) {
         assert(err)
         done()
       })
     })
 
     it('should add a string-type property', function(done) {
-      updater(destPackagePath).add('time', 'now', function(err) {
+      updater(destSettingsPath).add('time', 'now', function(err) {
         if (err) return done(err)
-        var pkg = JSON.parse(fs.readFileSync(destPackagePath))
+        var pkg = JSON.parse(fs.readFileSync(destSettingsPath))
         assert.equal('now', pkg.time)
         done()
       })
     })
 
     it('should add an array-type property', function(done) {
-      updater(destPackagePath).add('numbers', [1, 2, 3], function(err) {
+      updater(destSettingsPath).add('numbers', [1, 2, 3], function(err) {
         if (err) return done(err)
-        var pkg = JSON.parse(fs.readFileSync(destPackagePath))
+        var pkg = JSON.parse(fs.readFileSync(destSettingsPath))
         assert.equal(3, pkg.numbers.length)
         assert.equal(3, pkg.numbers[2])
         done()
@@ -47,7 +47,7 @@ describe('updater()', function() {
     })
 
     it('should add an object-type property', function(done) {
-      updater(destPackagePath).add('compound', {
+      updater(destSettingsPath).add('compound', {
         'one': '4.14.1',
         'two': [1, 2, 3],
         'three': {
@@ -56,10 +56,19 @@ describe('updater()', function() {
         }
       }, function(err) {
         if (err) return done(err)
-        var pkg = JSON.parse(fs.readFileSync(destPackagePath))
+        var pkg = JSON.parse(fs.readFileSync(destSettingsPath))
         assert.equal('4.14.1', pkg.compound.one)
         assert.equal(1, pkg.compound.two[0])
         assert.equal('a', pkg.compound.three.a)
+        done()
+      })
+    })
+
+    it('should support dot selector', function(done) {
+      updater(destSettingsPath).add('animals.mammals.cats', [], function(err) {
+        if (err) return done(err)
+        var pkg = JSON.parse(fs.readFileSync(destSettingsPath))
+        assert(Array.isArray(pkg.animals.mammals.cats))
         done()
       })
     })
@@ -69,25 +78,25 @@ describe('updater()', function() {
   describe('.update()', function() {
 
     it('should throw if a property is not defined', function(done) {
-      updater(destPackagePath).update('random', '', function(err) {
+      updater(destSettingsPath).update('random', '', function(err) {
         assert(err)
         done()
       })
     })
 
     it('should overwrite an existing string-type property', function(done) {
-      updater(destPackagePath).update('license', 'FREE', function(err) {
+      updater(destSettingsPath).update('license', 'FREE', function(err) {
         if (err) return done(err)
-        var pkg = JSON.parse(fs.readFileSync(destPackagePath))
+        var pkg = JSON.parse(fs.readFileSync(destSettingsPath))
         assert.equal('FREE', pkg.license)
         done()
       })
     })
 
     it('should append to an existing array-type property', function(done) {
-      updater(destPackagePath).update('tags', 'cool', function(err) {
+      updater(destSettingsPath).update('tags', 'cool', function(err) {
         if (err) return done(err)
-        var pkg = JSON.parse(fs.readFileSync(destPackagePath))
+        var pkg = JSON.parse(fs.readFileSync(destSettingsPath))
         assert(pkg.tags.includes('node'))
         assert(pkg.tags.includes('cool'))
         done()
@@ -95,13 +104,22 @@ describe('updater()', function() {
     })
 
     it('should append to an existing object-type property', function(done) {
-      updater(destPackagePath).update('author', {
+      updater(destSettingsPath).update('author', {
         'username': 'hacksparrow'
       }, function(err) {
         if (err) return done(err)
-        var pkg = JSON.parse(fs.readFileSync(destPackagePath))
+        var pkg = JSON.parse(fs.readFileSync(destSettingsPath))
         assert.equal('Hage Yaapa', pkg.author.name)
         assert.equal('hacksparrow', pkg.author.username)
+        done()
+      })
+    })
+
+    it('should support dot selector', function(done) {
+      updater(destSettingsPath).update('author.hobbies.primary', 'ping pong', function(err) {
+        if (err) return done(err)
+        var pkg = JSON.parse(fs.readFileSync(destSettingsPath))
+        assert(pkg.author.hobbies.primary.includes('ping pong'))
         done()
       })
     })
@@ -111,27 +129,36 @@ describe('updater()', function() {
   describe('.delete()', function() {
 
     it('should throw if a property is not defined', function(done) {
-      updater(destPackagePath).delete(['name', 'random'], function(err) {
+      updater(destSettingsPath).delete(['name', 'random'], function(err) {
         assert(err)
         done()
       })
     })
 
     it('should delete a single property', function(done) {
-      updater(destPackagePath).delete('name', function(err) {
+      updater(destSettingsPath).delete('name', function(err) {
         if (err) return done(err)
-        var pkg = JSON.parse(fs.readFileSync(destPackagePath))
+        var pkg = JSON.parse(fs.readFileSync(destSettingsPath))
         assert.equal(undefined, pkg.name)
         done()
       })
     })
 
     it('should delete multiple properties', function(done) {
-      updater(destPackagePath).delete(['name', 'version'], function(err) {
+      updater(destSettingsPath).delete(['name', 'version'], function(err) {
         if (err) return done(err)
-        var pkg = JSON.parse(fs.readFileSync(destPackagePath))
+        var pkg = JSON.parse(fs.readFileSync(destSettingsPath))
         assert(!('name' in pkg))
         assert(!('version' in pkg))
+        done()
+      })
+    })
+
+    it('should support dot selector', function(done) {
+      updater(destSettingsPath).delete('author.hobbies.secondary', function(err) {
+        if (err) return done(err)
+        var pkg = JSON.parse(fs.readFileSync(destSettingsPath))
+        assert(!('secondary' in pkg.author.hobbies))
         done()
       })
     })
